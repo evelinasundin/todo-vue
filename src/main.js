3f8ab9f0ddb3eb2c0e2ad251cc0234ce0d6bcfe0
evelinasundin <evelinasundin@hotmail.se>
2017-10-03 14:06:11 +0200

    remove and adding todos

--- src/main.js
+++ src/main.js
@@ -1,15 +1,34 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+// import App from './App'
+// import router from './router'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   router,
+//   template: '<App/>',
+//   components: { App }
+// })
+
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+  el: '.todoapp',
+  data () {
+    return {
+      newTodo: '',
+      todos: [{id: 0, title: 'New Todo', completed: 'false'}]
+    }
+  },
+  methods: {
+    addTodo () {
+      this.todos.push({title: this.newTodo, completed: 'false', id: this.todos.length})
+      this.newTodo = ''
+    },
+    removeTodo (todo) {
+      this.todos.splice(this.todos.indexOf(todo), 1)
+    }
+  }
 })
